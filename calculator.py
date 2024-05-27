9939b61bf8ab5b2a09f330bbc01d2f8784a8743c
Артём Рыжов <ar.ryqp@mail.ru>
2024-05-27 15:16:46 +0300

    add bin in dec

--- calculator.py
+++ calculator.py
@@ -1,7 +1,8 @@
 import math
 
 
-def dec_in_bin(number: int):
+def dec_in_bin(number: int) -> str:
+    """Преобразует десятичное число в двоичное"""
     remain_number = number
     binary_number = ''
     max_power = int(math.floor(math.log(number, 2)))
@@ -15,4 +16,18 @@ def dec_in_bin(number: int):
             bit = 0
         print("{pow} степень - {bit} {dec}".format(pow=i, bit=bit, dec=2**i*bit))
     print("{} в двоичном виде: {}, длина {}".format(number, binary_number, len(binary_number)))
+    return binary_number
 
+
+def bin_in_dec(bin_number: str) -> int:
+    """Преобразует двоичное число в десятичное"""
+    dec_number = 0
+    res = ''
+    resDecimal = ''
+    for i in range(len(bin_number)):
+        if bin_number[i] == '1':
+            dec_number += 2 ** (len(bin_number) - i - 1)
+            res += '2^{}+'.format(len(bin_number) - i - 1)
+            resDecimal += '{}+'.format(2**(len(bin_number) - i - 1))
+    print('{} = {} = {}'.format(res[0:-1], resDecimal[0:-1], dec_number))
+    return dec_number
